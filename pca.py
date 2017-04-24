d6b7826d977f55211477bf2903c8afca48550dfc
sayeed abdurrahman siddiqui <siddiqui.77@sl7.cse.ohio-state.edu>
2017-04-24 05:30:56 -0400

    Finished preprocessing

--- pca.py
+++ pca.py
@@ -1,8 +1,18 @@
 # Sayeed Siddiqui
 # Perform PCA on College Scorecard data for a simplified dataset
 
-from sklearn.decomposition import PCA
+import pandas as pd
+import numpy as np
+import scipy.linalg as la 
 
-data = pandas.read_csv('cleaned.csv')
-pca = PCA(n_components=2)
-pca.fit(data)
+Z = pd.read_csv('processed.csv').as_matrix()
+
+cov = np.cov(Z, rowvar=False)
+w, u = la.eigh(cov)
+
+# Sort values in decreasing order
+ind = np.argsort(w)[::-1]
+u = u[:,ind]
+w = w[ind]
+
+
--- preprocess.py
+++ preprocess.py
@@ -5,19 +5,54 @@
 import pandas as pd
 
 dat1 = pd.read_csv('../Downloads/Most-Recent-Cohorts-All-Data-Elements.csv')
+dat1.drop(dat1.columns[0], axis=1, inplace=True)
 
 # Treat privacy-suppressed as missing values (only do once)
 #dat2 = dat1.replace('PrivacySuppressed', '')
 #dat2.to_csv('../Downloads/Most-Recent-Cohorts-All-Data-Elements.csv')
 
 # Select only numeric variables
-dat2 = dat1.select_dtypes(include=['int64', 'float64'])
+dat2 = dat1.select_dtypes(include=['float64', 'int64'])
 
 # Drop columns which are mostly null or zero
 nulls = dat2.isnull().sum()
 zeros = dat2.eq(0).sum()
 dat3 = dat2.loc[:, nulls + zeros < len(dat2)/2]
 
+# Coalesce related attributes into one
+def rep(lst):
+	# First sort reverse-alphabetically to get highest year number
+	lst.sort(reverse=True)
+	# Next sort by string length to get simplest
+	lst.sort(key=lambda x: len(x))
+	return lst[0]
 
+attrs = list(dat3.columns)
+groups = {}
+for attr in attrs:
+	prefix = attr[0:7] 
+	if prefix in groups:
+		groups[prefix].append(attr)
+	else:
+		groups[prefix] = [attr]
+attrs = list(map(rep, groups.values()))
+dat4 = dat3[attrs]
 
-# ['OPEID', 'OPEID6', 
+# Specify excluded columns
+excluded = ['UNITID', 'OPEID', 'OPEID6', 'SCH_DEG', 'MAIN', 'CONTROL', 'ST_FIPS', 'REGION', 'LOCALE', 'CCBASIC', 'CCUGPROF', 'CCSIZSET', 'RELAFFIL', 'CURROPER', 'POOLYRS']
+
+# Build list of dependent variables 
+dependent = []
+tokens = ['CDR', 'RPY', 'DEBT', 'REPAY', 'EARN', 'GT']
+# This function tests whether an attribute should be dependent
+dep = lambda a: any(map(lambda x: x in a, tokens))
+dependent = [attr for attr in attrs if dep(attr)]
+excluded.extend(dependent)
+
+dat5 = dat4.drop(excluded, axis=1)
+
+# Normalize data
+dat6= dat5.fillna(dat5.mean())
+dat6 = (dat6 - dat6.mean())/dat6.std()
+
+dat6.to_csv('processed.csv')
