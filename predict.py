d94a458d7d754d32e809c6b674fb10a064aba036
Sayeed Siddiqui <sasidiki@gmail.com>
2017-04-24 17:28:54 -0400

    done

--- predict.py
+++ predict.py
@@ -7,7 +7,8 @@ import time
  
 from sklearn.neural_network import MLPRegressor
 from sklearn.svm import SVR
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.kernel_ridge import KernelRidge
  
 from sklearn.model_selection import GridSearchCV
 
@@ -17,22 +18,29 @@ Y = pd.read_csv('data/Y.csv', header=None).as_matrix().ravel()
 preds = [(LinearRegression(),
             {}),
         (MLPRegressor(),
-            {'activation': ['identity','logistic','tanh','relu'],
+            {'activation': ['logistic','tanh','relu'],
              'hidden_layer_sizes': [[10],[7],[4]],
-             'max_iter': [200, 500]}),
+             'max_iter': [200]}),
         (SVR(),
             {'kernel': ['rbf','poly'],
              'degree': [1,2],
-			 'C': [.5, 1]})]
+			 'C': [.5, 1]}),
+        (Lasso(),
+            {'max_iter': [100, 500, 1000],
+             'alpha' : [.5, 1, 2]}),
+        (KernelRidge(),
+            {'kernel': ['rbf', 'poly'],
+             'degree' : [2,3],
+             'alpha' : [1, 2]})]
 
-tpred, tparams = preds[2]
+tpred, tparams = preds[4]
 
 # Try all predictors, combinations of parameters, and different scores 
 def evaluate(pred, params):
  
     for score in ['r2', 'neg_mean_squared_error']:
         start = time.time()
-        CV = GridSearchCV(pred, params, cv=10, scoring=score)
+        CV = GridSearchCV(pred, params, cv=3, scoring=score)
         CV.fit(X, Y)
  
         print('%s was %0.2f with params: %s in %f s' % (score, CV.best_score_, str(CV.best_params_), time.time()-start))
